5ceb7be0c6e6701335e5a3e0ba4379fc9339902d
Isaac Yung <isaaczoi@gmail.com>
2017-09-07 14:27:50 -0300

    Routes

--- config/config.exs
+++ config/config.exs
@@ -3,4 +3,6 @@ use Mix.Config
 import_config "#{Mix.env}.exs"
 
 config :polycode_api, ecto_repos: [PolycodeApi.Repo]
-config :maru, PolycodeApi.Base, http: [port: 8880]
+
+config :maru, PolycodeApi.Base,
+        http: [port: 8880]
--- lib/polycode_api/api/language.ex
+++ lib/polycode_api/api/language.ex
@@ -1,7 +1,39 @@
 defmodule PolycodeApi.API.Language do
   use Maru.Router
 
-  get do
-    json(conn, %{ hello: :world })
+  namespace :language do
+    namespace ":language" do
+      desc "Retrieve a language datas"
+      params do
+        requires :language, type: String
+      end
+      get do
+        json(conn, %{ lang: params[:language] })
+      end
+
+      desc "Insert language datas"
+      params do
+        requires :language, type: String
+      end
+      post do
+        json(conn, %{ lang: params[:language] })
+      end
+
+      desc "Insert language datas"
+      params do
+        requires :language, type: String
+      end
+      put do
+        json(conn, %{ lang: params[:language] })
+      end
+
+      desc "Insert language datas"
+      params do
+        requires :language, type: String
+      end
+      patch do
+        json(conn, %{ lang: params[:language] })
+      end
+    end
   end
 end
